9a345176548a34aa74658339471ad7676f56b310
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-07-27 11:29:43 -0500

    feat: added decorators to pokemon dto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsString, MinLength, IsInt, Min, IsPositive } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsInt()
+  @Min(1)
+  @IsPositive()
+  numPokemon: number;
+}
