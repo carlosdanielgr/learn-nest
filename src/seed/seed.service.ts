5ab322faea62ada9de47f84dd7f7692fbe2347fd
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-07-27 16:08:32 -0500

    feat: insert data pokemon-api completed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { IPokeResponse } from './interfaces/poke-res.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'seed executed';
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  async executeSeed() {
+    const { data } = await this.axios.get<IPokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(({ url, name }) => {
+      const segments = url.split('/');
+      const numPokemon = +segments[segments.length - 2];
+      this.pokemonService.create({ name, numPokemon });
+    });
+
+    return `Data created successfully`;
   }
 }
