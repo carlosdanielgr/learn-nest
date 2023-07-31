aa2289e76199c5f5c53918b30d0c0c29292c2b2b
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-07-31 15:50:00 -0500

    feat: connecto to db completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const { env } = process;
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.DB_HOST,
+      port: +env.DB_PORT,
+      database: env.DB_NAME,
+      username: env.DB_USERNAME,
+      password: env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
