4846f3b10407ab401520daa84aaf71ca73bffe47
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-03 13:00:05 -0500

    feat: create teslo completed

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleError(error);
+    }
   }
 
   findAll() {
@@ -22,5 +36,10 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleError(error): void {
+    console.log(error?.detail ? error.detail : error);
+    throw new InternalServerErrorException('Review console');
   }
 }
