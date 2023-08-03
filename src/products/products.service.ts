b837ad8b8115fe4ca3bd6b82ea9abb680ea0a9a2
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-08-03 14:08:03 -0500

    feat: get, get by id and delete completed

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -33,19 +33,24 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    try {
+      return await this.productRepository.findOneBy({ id });
+    } catch (error) {
+      this.handleError(error);
+    }
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    return await this.productRepository.remove(product);
   }
 }
